f053f22cf9b844880b058b33c4b1eae51859c02b
Alan Daniel <20223tn018@utez.edu.mx>
2025-05-20 15:52:50 -0600

    Repositories upload

---
+++ RunRunBackEnd/src/main/java/utez/edu/mx/runrunbackend/RunRunBackEndApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.runrunbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunRunBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunRunBackEndApplication.class, args);
+    }
+
+}
---
+++ RunRunBackEnd/src/test/java/utez/edu/mx/runrunbackend/RunRunBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.runrunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunRunBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
