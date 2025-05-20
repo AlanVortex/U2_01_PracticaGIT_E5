abc7078b285d425767405438319a9f317ad19ae7
MarmolejoValle <20223tn132@utez.edu.mx>
2025-05-20 16:24:26 -0600

    add auto increment Car and Brand Fixes #2

--- RunRunBackEnd/src/main/java/utez/edu/mx/runrunbackend/models/car/CarEntity.java
+++ RunRunBackEnd/src/main/java/utez/edu/mx/runrunbackend/models/car/CarEntity.java
@@ -1,14 +1,12 @@
 package utez.edu.mx.runrunbackend.models.car;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import utez.edu.mx.runrunbackend.models.brand.BrandEntity;
 
 @Entity
 public class CarEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
