4d14e869174a465d05cec095ac51e54762395ab5
Alan Daniel <20223tn018@utez.edu.mx>
2025-05-23 13:20:13 -0600

    Add of best practices and the name of the window

--- RunRunFrontEnd/src/App.jsx
+++ RunRunFrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Autos from './pages/autos.jsx';
-import Proveedores from './pages/proveedores.jsx';
+import Autos from './pages/Automoviles.jsx';
+import Proveedores from './pages/Proveedores.jsx';
 import VerProveedor from './pages/VerProveedor.jsx'; // correcto
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <div className="p-4">
           <Routes>
             <Route path="/proveedores" element={<Proveedores />} />
-            <Route path="/proveedores/:id" element={<VerProveedor />} /> {/* sin comillas */}
+            <Route path="/proveedores/:id" element={<VerProveedor />} />
             <Route path="/automoviles" element={<Autos />} />
           </Routes>
         </div>
